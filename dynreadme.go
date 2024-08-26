d5bea07e3a3daf3aa2c6aea1c2ce84f6b4682d66
Naveen Prashanth <78990165+gnpaone@users.noreply.github.com>
2024-08-26 23:06:27 +0530

    Update dynreadme.go

--- dynreadme.go
+++ dynreadme.go
@@ -11,8 +11,8 @@ import (
 // Update is a type for updating readme content
 type Update struct{}
 
-// updateContent parses and updates content between markers
-func (u *Update) updateContent(readmePath, markerText, mdText, isTable, tableOptions string) error {
+// UpdateContent parses and updates content between markers
+func (u *Update) UpdateContent(readmePath, markerText, mdText, isTable, tableOptions string) error {
 	readmeContent, err := os.ReadFile(readmePath)
 	if err != nil {
 		return err
