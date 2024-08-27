51ef1bfb0b8bb82bc2342843d822334a51cfc495
Naveen Prashanth <78990165+gnpaone@users.noreply.github.com>
2024-08-27 12:50:16 +0530

    Update main.go

--- action/main.go
+++ action/main.go
@@ -8,7 +8,7 @@ import (
 	"os/exec"
 	"errors"
 
-	dynreadme "github.com/gnpaone/dynamic-update-readme"
+	. "github.com/gnpaone/dynamic-update-readme"
 )
 
 func main() {
@@ -27,47 +27,47 @@ func main() {
 
 	// updater := dynreadme.Update{}
 
-	if err := dynreadme.UpdateContent(readmePath, markerText, mdText, isTable, tableOptions); err != nil {
+	if err := UpdateContent(readmePath, markerText, mdText, isTable, tableOptions); err != nil {
 		log.Fatalf("Failed to update README: %s", err)
 	}
 
-	if err := updateGitRepo(readmePath, commitMessage, gitUsername, gitEmail, confirmAndPush); err != nil {
-		log.Fatalf(err)
+	return updateGitRepo(readmePath, commitMessage, gitUsername, gitEmail, confirmAndPush); err != nil {
+		log.Fatal(err)
 	}
 }
 
 func updateGitRepo(readmePath, commitMessage, gitUsername, gitEmail, confirmAndPush string) error {
 	safeCmd := exec.Command("git", "config", "--global", "--add", "safe.directory", "/github/workspace")
 	if err := safeCmd.Run(); err != nil {
-		return errors.New("Error setting safe directory: %w", err)
+		log.Fatalf("Error setting safe directory: %q", err)
 	}
 
 	nameCmd := exec.Command("git", "config", "user.name", gitUsername)
 	if err := nameCmd.Run(); err != nil {
-		return errors.New("Error setting git user: %w", err)
+		log.Fatalf("Error setting git user: %q", err)
 	}
 
 	emailCmd := exec.Command("git", "config", "user.email", gitEmail)
 	if err := emailCmd.Run(); err != nil {
-		return errors.New("Error setting git email: %w", err)
+		log.Fatalf("Error setting git email: %q", err)
 	}
 
 	statusCmd, err := exec.Command("git", "status").Output()
 	if err != nil {
-		return errors.New("Error getting status: %w", err)
+		log.Fatalf("Error getting status: %q", err)
 	}
 
 	statusOutput := string(statusCmd)
 	if !strings.Contains(statusOutput, "nothing to commit") {
 		if err := exec.Command("git", "add", readmePath).Run(); err != nil {
-			return errors.New("Error adding to staging area: %w", err)
+			log.Fatalf("Error adding to staging area: %q", err)
 		}
 		if err := exec.Command("git", "commit", "-m", commitMessage).Run(); err != nil {
-			return errors.New("Error committing to repo: %w", err)
+			log.Fatalf("Error committing to repo: %q", err)
 		}
 		if confirmAndPush == "true" {
 			if err := exec.Command("git", "push").Run(); err != nil {
-				return errors.New("Error pushing to repo: %w", err)
+				log.Fatalf("Error pushing to repo: %q", err)
 			}
 		} else if confirmAndPush == "false" {
 			output := fmt.Sprintf("git_username=%s\ngit_email=%s\ncommit_message=%s\n", gitUsername, gitEmail, commitMessage)
@@ -75,18 +75,18 @@ func updateGitRepo(readmePath, commitMessage, gitUsername, gitEmail, confirmAndP
 		}
 	}
 
-	return nil
+	// return nil
 }
 
 func appendToFile(filename, text string) error {
 	file, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
 	if err != nil {
-		return errors.New("Error opening file: %w", err)
+		log.Fatalf("Error opening file: %q", err)
 	}
 	defer file.Close()
 
 	if _, err := file.WriteString(text); err != nil {
-		return errors.New("Error writing to file: %w", err)
+		log.Fatalf("Error writing to file: %q", err)
 	}
-	return nil
+	// return nil
 }
